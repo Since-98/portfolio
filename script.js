5a7ad0f86c948a79fa17bcda355525b4ea4e4b20
Since-98 <farhiya@putlook.com>
2024-07-12 16:16:11 +0200

    portfolio

---
+++ script.js
@@ -0,0 +1,10 @@
+// Your interactive JS magic here, babe! 😘
+document.querySelectorAll('nav ul li a').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
